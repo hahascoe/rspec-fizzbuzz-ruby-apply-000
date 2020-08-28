dbd758a2340f849b88fdb5327d1cef39634500a8
Harrison Hascoe <hascoeharrison@gmail.com>
2020-08-28 19:55:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
+  if int % 5 && 3 == 0
+    puts "BuzzFizz"
+  else 
+    puts "nil"
+  
   
 end
